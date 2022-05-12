01e39f319a11d95ab5cb45dc65de71e7e71239fb
AlphonsaJo <91889313+AlphonsaJo@users.noreply.github.com>
2022-05-12 23:30:41 +0530

    Create factorial_basic.c

---
+++ factorial_basic.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//function definition and declaration
+//A variable or a function can be declared any number of times
+//A variable or a function can be defined only once
+int fact(int n)
+{   int i, sum=0;
+    int f=1;
+    for(i=1; i<=n; i++)
+    {  
+        f=i*f;
+        sum=+f;
+    }
+return sum;
+}
+
+
+int main()
+{   int x=5;
+    printf("the factorial of %d is %d\n ", x, fact(x));
+}
